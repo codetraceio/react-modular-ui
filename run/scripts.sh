b0848bc49b8d8d68200ff4ff93c0d0ad1db59d22
omegascorp <omegascorp@gmail.com>
2017-07-12 20:57:49 +0400

    Add amd build.

--- run/scripts.sh
+++ run/scripts.sh
@@ -5,6 +5,10 @@
 --declaration --declarationDir "./typescript" \
 ./src/index.ts
 
+./node_modules/typescript/bin/tsc --outDir "./dist/amd" --target ES5 --noImplicitAny \
+--module amd --moduleResolution classic --jsx react --allowSyntheticDefaultImports \
+./src/index.ts
+
 ./node_modules/typescript/bin/tsc --outDir "./dist" --target ES5 --noImplicitAny \
 --module commonjs --moduleResolution classic --jsx react --allowSyntheticDefaultImports \
 ./src/static.ts ./src/example/client/*.ts ./src/example/client/*.tsx
