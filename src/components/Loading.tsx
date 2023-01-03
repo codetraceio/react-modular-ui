587c9abbae61b053c781366dab3ba3ec0a664e21
omegascorp <omegascorp@gmail.com>
2023-01-03 16:27:17 +0400

    update loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,33 +1,26 @@
-import * as React from "react";
+import React from "react";
 
-import { Modifiers, getBlockName } from "../services/componentService";
+import { className } from "../utils/className";
 
 export interface LoadingProps {
   size?: string | number;
   color?: string;
 }
 
-export default class Loading extends React.PureComponent<LoadingProps, {}> {
-  getModifierObject(): Modifiers {
-    return {
-      size: this.props.size,
-      color: this.props.color
-    };
-  }
-
-  render() {
-    return (
-      <div
-        className={getBlockName("loading", this.getModifierObject())}
-      >
-        <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-          <circle cx="50" cy="50" r="40" stroke="none" fill="none" strokeWidth="10">
-            <animate attributeName="stroke-dashoffset" dur="2s" repeatCount="indefinite" from="0" to="-502"/>
-            <animate attributeName="stroke-dasharray" dur="2s" repeatCount="indefinite"
-                     values="150.6 100.4;1 250;150.6 100.4"/>
-          </circle>
-        </svg>
-      </div>
-    );
-  }
+export default function Loading(props: LoadingProps) {
+  return (
+    <div
+      className={className("loading")}
+      data-size={props.size}
+      data-color={props.color}
+    >
+      <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" r="40" stroke="none" fill="none" strokeWidth="10">
+          <animate attributeName="stroke-dashoffset" dur="2s" repeatCount="indefinite" from="0" to="-502"/>
+          <animate attributeName="stroke-dasharray" dur="2s" repeatCount="indefinite"
+                    values="150.6 100.4;1 250;150.6 100.4"/>
+        </circle>
+      </svg>
+    </div>
+  );
 }
