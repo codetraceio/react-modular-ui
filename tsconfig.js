4f377289eae6f71bd2e14e4782eebd69e56d4974
omegascorp <omegascorp@gmail.com>
2023-01-06 14:17:00 +0400

    update build

--- tsconfig.js
+++ tsconfig.js
@@ -2,9 +2,10 @@ module.exports = {
   "target": "ES5",
   "lib": ["ES6", "dom"],
   "noImplicitAny": true,
-  "module": "commonjs",
-  "moduleResolution": "classic",
+  "module": "esnext",
+  "moduleResolution": "node",
   "jsx": "react",
   "allowSyntheticDefaultImports": true,
-  "allowJs": true
+  "allowJs": true,
+  "esModuleInterop": true,
 };
